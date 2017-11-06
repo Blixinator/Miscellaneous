7dbe2f00c511a5b917d9629559aef4d591561812
Blixinator <blixtslag@gmail.com>
2017-11-06 02:21:30 -0600

    Deleting branch
    Saving so I can delete branch and remake it later

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,9 +1,9 @@
 
 from Tkinter import *
 import random
-import operator
+# import operator
 import time
-import tkFont as tkfont
+import tkFont
 
 def printf(s):
 	print s
@@ -71,12 +71,12 @@ class App:
 		# self.master.bind("<KeyPress>", lambda event: self.shift_key(event, True))
 		# self.master.bind("<KeyRelease>", lambda event: self.shift_key(event, False))
 
-		self.bold_font = tkfont.Font(family="Helvetica", size=12, weight="bold")
+		self.bold_font = tkFont.Font(family="Helvetica", size=12, weight="bold")
 
 		l = Label(f, text = "TEST1234567890\nTEST\nTEST", bg='red')
 
 
-		l.grid(row=1, column=0)
+		# l.grid(row=1, column=0)
 		
 
 		self.row_entry = Entry(f)
@@ -110,6 +110,10 @@ class App:
 
 	def populate(self, rows, columns, bombs, click_position=None):
 		self.reset()
+		possible_bomb_locations = range(0, self.rows*self.columns)
+		if click_position != None:
+			clicked_r, clicked_c = click_position
+			possible_bomb_locations.remove(clicked_c*clicked_r+clicked_c)
 
 		for widget in self.button_frame.winfo_children():
 			widget.destroy()
@@ -120,21 +124,22 @@ class App:
 		"""Place bombs
 		Select a random spot on the map. Add a bomb if there isn't already there. Increment the surrounding tiles"""
 		bombs_placed = 0
+		
 		while bombs_placed < bombs:
-			r = random.randint(0, rows-1)
-			c = random.randint(0, columns-1)
-			if self.map[r][c] != "*" and (r,c)!=click_position:
-				self.map[r][c] = "*"
-				for i in xrange(-1,2):
-					for j in xrange(-1,2):
-						if (i==0 and j==0) or not(0<=r+i<self.rows) or not(0<=c+j<self.columns):
-							continue
-						try:
-							if self.map[r+i][c+j]!="*":
-								self.map[r+i][c+j]+=1
-						except IndexError:
-							""
-				bombs_placed += 1
+			bomb_position = random.choice(possible_bomb_locations)
+			possible_bomb_locations.remove(bomb_position)
+			r = int(bomb_position/( self.rows))
+			c = bomb_position % self.columns
+			self.map[r][c] = "*"
+			for i in xrange(-1,2):
+				for j in xrange(-1,2):
+					if (i==0 and j==0) or not(0<=r+i<self.rows) or not(0<=c+j<self.columns):
+						continue
+			
+					if self.map[r+i][c+j]!="*":
+						self.map[r+i][c+j]+=1
+
+			bombs_placed += 1
 
 		"""Create Buttons"""
 		for r in xrange(0, rows):
@@ -260,12 +265,15 @@ class App:
 
 		"""If all non-bomb spaces are revealed"""
 		if (self.rows*self.columns == self.bombs + self.revealed):
-			print "YOU WIN"
+			print "YOU WIN (1)"
 			self.win_condition()
 
 		"""If a bomb is clicked"""
 		if self.map[r][c]=="*":
 			print "BOOM!"
+			self.buttons[r][c]['bg']='red'
+			self.sink(r,c)
+			self.buttons[r][c]['relief']='sunken'
 			for row in xrange(0, self.rows):
 				for column in xrange(0, self.columns):
 					if self.map[row][column] == "*":
@@ -349,6 +357,7 @@ class App:
 	
 			if self.left_mouse_down and self.right_mouse_down:
 				# print 'both down'
+				"BOTH DOWN"
 				if self.buttons[r][c]['state']!='disabled':
 					if self.revealed_map[r][c]==True:
 						"""Count the number of flags in the adjacent tiles"""
@@ -360,7 +369,7 @@ class App:
 								if self.buttons[r+i][c+j]['state']=='disabled':
 									flag_count+=1
 						"""If the number of flags is equal to the tile number, reveal the surrounding tiles"""
-						if str(flag_count) == str(self.buttons[r][c]['text']):
+						if str(flag_count) >= str(self.buttons[r][c]['text']):
 							for i in xrange(-1,2):
 								for j in xrange(-1,2):
 									if (i==0 and j==0) or not(0<=r+i<self.rows) or not(0<=c+j<self.columns) or self.buttons[r+i][c+j]['state']=='disabled':
@@ -370,11 +379,13 @@ class App:
 
 			elif self.left_mouse_down:
 				# print 'left down'
+				"LEFT DOWN"
 				if self.buttons[r][c]['state']!='disabled':
 					self.button_click(r,c)
 
 			elif self.right_mouse_down:
 				# print 'right down'
+				"RIGHT DOWN"
 				if self.revealed_map[r][c]==False:
 					self.set_flag(r,c)
 				else:
@@ -392,8 +403,10 @@ class App:
 		if self.buttons[r][c]['state']=="normal":
 			self.buttons[r][c].config(state="disabled", text="F")
 			self.flags+=1
+
+			"""I don't think this condition is ever met"""
 			if (self.flags == self.bombs) and (self.rows*self.columns == self.flags + self.revealed):
-				print "YOU WIN"
+				print "YOU WIN (2)"
 				self.win_condition()
 
 
@@ -402,7 +415,7 @@ class App:
 			self.flags-=1
 
 	def win_condition(self):
-		print "!"
+		# print "!"
 		for r in xrange(0, self.rows):
 			for c in xrange(0, self.columns):
 				b = self.buttons[r][c]
@@ -412,7 +425,7 @@ class App:
 				b.unbind("<ButtonRelease-3>")
 				b.unbind("<Leave>")
 				b.unbind("<B1-Motion>")
-				b.config(command = None)
+				b.config(command = "")
 
 
 
